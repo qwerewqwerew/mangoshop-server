6e7b5c6127d4f0b043959d13dc560caf229ad18f
qwerewqwerew <qwerew0@gmail.com>
2022-07-10 15:18:00 +0900

    리액트 클라이언트와 DB 연동

--- server.js
+++ server.js
@@ -1,38 +1,79 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const models = require("./models");
 const port = 8080;
 
 app.use(express.json()); //json 형식의 데이터를 처리할 수 있게 설정하는 코드
 app.use(cors()); //브라우저의 CORS 이슈를 막기 위해 사용하는 코드
 
-app.get("/products", async (req, res) => {
-	const query = req.query; // query 상수에 get 방식으로 /products 경로에 들어온 요청을 저장
-	console.log("Query:", query);
-	res.send({
-		products: [
-			{ id: 1, name: "습식사료", price: 10000, seller: "내추럴코어", imageUrl: "images/products/food1.jpg" },
-			{ id: 2, name: "하네스", price: 50000, seller: "도기멍", imageUrl: "images/products/acc1.jpg" },
-			{ id: 3, name: "배변패드", price: 30000, seller: "흡수혁명", imageUrl: "images/products/house1.jpg" },
-		],
-	});
+app.get("/products", (req, res) => {
+	models.Product.findAll({
+		order: [["createdAt", "DESC"]],
+		attributes: ["id", "name", "price", "seller","imageUrl", "createdAt" ],
+	})
+		.then((result) => {
+			console.log("PRODUCTS : ", result);
+			res.send({
+				products: result,
+			});
+		})
+		.catch((error) => {
+			console.error(error);
+			res.send("에러 발생");
+		});
 });
 
-app.post("/products", async (req, res) => {
+app.post("/products", (req, res) => {
 	const body = req.body;
-	res.send({
-		body,
-	});
+	const { name, description, price, seller } = body;
+	if (!name || !description || !price || !seller) {
+		res.send("모든 필드를 입력해주세요");
+	}
+	models.Product.create({
+		name,
+		description,
+		price,
+		seller,
+	})
+		.then((result) => {
+			console.log("상품생성결과:", result);
+			res.send({ result });
+		})
+		.catch((error) => {
+			console.error(error);
+			res.send("상품 업로드에 문제가 발생했습니다.");
+		});
 });
 
+app.get("/products/:id", (req, res) => {
+	const params = req.params;
+	const { id } = params;
+	models.Product.findOne({
+		where: {
+			id: id,
+		},
+	})
+		.then((result) => {
+			return res.send({ product: result });
+		})
+		.catch((error) => {
+			console.error(error);
+			res.send("상품조회시 에러가 발생했습니다.");
+		});
+});
 
-app.get("/products/:id/events/:eventId",(req,res)=>{
-  const params = req.params;
-  const {id,eventId} = params;
-  res.send(`id는 ${id} 와 ${eventId}입니다`)
-})
-
-//세팅한 app을 실행시킨다.
 app.listen(port, () => {
-	console.log("망고샵의 쇼핑몰 서버가 돌아가고 있습니다.");
+	console.log("망고샵의 서버가 구동중 입니다.");
+	models.sequelize
+		.sync()
+		.then(() => {
+			console.log("✓ DB 연결 성공");
+		})
+		.catch(function (err) {
+			console.error(err);
+			console.log("✗ DB 연결 에러");
+			//에러발생시 서버프로세스 종료
+			process.exit();
+		});
 });
